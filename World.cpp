9c5d491910028d74f8a39e6b137e9b14ee50a22e
ivaanms10 <ivanmorsan15@gmail.com>
2026-05-20 18:17:29 +0200

    World class constructor and destroyer

--- World.cpp
+++ World.cpp
@@ -1,2 +1,25 @@
 #include "World.h"
 
+/*
+	@brief Default constructor of the World class.
+*/
+World::World() {
+	createWorld();
+}
+
+
+/*
+	@brief Parameterized constructor of the World class.
+	@param entities Vector that contains the entities of the world.
+*/
+World::World(std::vector<Entity*> entities) : entities(entities){
+	createWorld();
+}
+
+
+/*
+	@brief Destroyer of the World class.
+*/
+World::~World() {
+	entities.clear();
+}
